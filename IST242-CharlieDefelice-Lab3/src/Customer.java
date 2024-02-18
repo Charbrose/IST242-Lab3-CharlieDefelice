8768230eec6b6091bbd4185973bc9c70c89fe944
Charbrose <124835997+Charbrose@users.noreply.github.com>
2024-02-18 18:56:40 -0500

    please work

--- IST242-CharlieDefelice-Lab3/src/Customer.java
+++ IST242-CharlieDefelice-Lab3/src/Customer.java
@@ -1,9 +1,11 @@
-public class Customer {
+public class Customer
+{
     private int id;
     private String name;
     private String email;
 
-    public Customer(int id, String name, String email) {
+    public Customer(int id, String name, String email)
+    {
         this.id = id;
         this.name = name;
         this.email = email;
